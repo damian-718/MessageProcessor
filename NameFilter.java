b0ae5b0b6876bccf77bedf7bf0fd37d26d7e9ef6
Damian Gunadasa <damian718@gmail.com>
2025-09-10 22:55:44 -0400

    initial commit

---
+++ NameFilter.java
@@ -0,0 +1,40 @@
+
+public class NameFilter implements Runnable{
+
+    private String[] names;
+    //ArrayList<String> names = new ArrayList<>();
+
+    public NameFilter(String[] names){
+        this.names = names;
+    }
+
+    @Override
+    public void run(){
+
+        // // think of this like for each name in names
+        // for (String name : names) {
+        //     if (name.length() > 3) {
+        //         String processed = name.toUpperCase();
+        //         System.out.println(processed);
+        //     }
+        // }
+
+        // names.length for array, length() for string length, size() for arraylist
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].length() > 3) {
+                String processed = names[i].toUpperCase();
+                System.out.println(processed);
+            }
+        }
+
+        
+    }
+    public static void main(String[] args) {
+        String names[] = {"Tom", "Jerry", "Amy", "Christopher", "Max", "John"};
+
+        NameFilter filter = new NameFilter(names);
+        Thread thread = new Thread(filter);
+
+        thread.start();
+    }
+}
