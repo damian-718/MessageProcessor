c853a93319e52cfb42ba356a11ca64737b2c26ef
Damian Gunadasa <damian718@gmail.com>
2025-09-23 17:21:29 -0400

    installed java11 and implemented spark proceccing locally

--- NameFilterSpark/src/main/java/com/myproject/NameFilterSpark.java
+++ NameFilterSpark/src/main/java/com/myproject/NameFilterSpark.java
@@ -1,32 +1,33 @@
 package com.myproject;
 
-import org.apache.spark.api.java.*;
-import org.apache.spark.SparkConf;
 import java.util.Arrays;
 import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 
 
 public class NameFilterSpark {
     public static void main(String[] args) {
-
-        // 1. Spark setup (local with all cores)
+        // Set Hadoop security property before any Spark/Hadoop classes are loaded
+        System.setProperty("hadoop.security.authentication", "Simple");
+        // ...existing code...
         SparkConf conf = new SparkConf().setAppName("NameFilterSpark").setMaster("local[*]");
-        JavaSparkContext sc = new JavaSparkContext(conf);
+        try (JavaSparkContext sc = new JavaSparkContext(conf)) {
+            // 2. Input data
+            List<String> names = Arrays.asList("Tom", "Jerry", "Amy", "Christopher", "Max", "John");
 
-        // 2. Input data
-        List<String> names = Arrays.asList("Tom", "Jerry", "Amy", "Christopher", "Max", "John");
+            // 3. Parallelize the list (Spark distributes it)
+            JavaRDD<String> namesRDD = sc.parallelize(names);
 
-        // 3. Parallelize the list (Spark distributes it)
-        JavaRDD<String> namesRDD = sc.parallelize(names);
+            // 4. Filter + transform
+            JavaRDD<String> processed = namesRDD
+                    .filter(name -> name.length() > 3)
+                    .map(String::toUpperCase);
 
-        // 4. Filter + transform
-        JavaRDD<String> processed = namesRDD
-                .filter(name -> name.length() > 3)
-                .map(String::toUpperCase);
-
-        // 5. Collect results back and print
-        processed.collect().forEach(System.out::println);
-
-        sc.close();
+            // 5. Collect results back and print
+            processed.collect().forEach(System.out::println);
+        }
     }
 }
